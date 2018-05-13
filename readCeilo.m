d997000a54cd15cf9871252042fb55466ff3dd25
killian <kbrennan@student.ethz.ch>
2018-05-13 12:06:00 +0200

    fixed read ceilo

--- readCeilo.m
+++ readCeilo.m
@@ -1,11 +1,7 @@
 
-clear;
+clear
 
-<<<<<<< HEAD
 list_dates = datenum(2018,3,12);
-=======
-list_dates = datenum(2018,4,13);
->>>>>>> origin/percipitation
 root_url = 'http://iacweb.ethz.ch/staff//krieger/data/FS18/Ceilometer/';
 
 
@@ -13,11 +9,7 @@ ceilo = read_ceilo_from_url(list_dates,root_url);
 aerosol_top = get_TCAL(list_dates);
 %% height vs time plot
 figure;
-<<<<<<< HEAD
 pcolor(ceilo.time,ceilo.range,log10(0.75*abs(ceilo.RCS)));
-=======
-pcolor(ceilo.time,ceilo.range,log10(abs(0.75*(ceilo.RCS))));
->>>>>>> origin/percipitation
 shading flat;
 hc = colorbar;
 colormap(jet);
@@ -25,108 +17,19 @@ caxis([2.4 5.5]);
 %set(gca, 'YScale', 'log')
 datetick;
 ylim([0 3000]);
-<<<<<<< HEAD
 hold on
 plot(ceilo.time,aerosol_top,'k', 'LineWidth',2)
 datetick;
 hold off
 %%
 clouds = getclouds(ceilo);
-=======
-title('Height vs Time Plot')
 
-%% clouds
-[cloudPresence, cloudHeight] = getclouds(ceilo);
-
-figure;
-area(ceilo.time,cloudPresence);
+area(ceilo.time,clouds);
 datetick;
-xlabel('Time UTC')
-ylabel('Presence of clouds')
-title('Presence of Clouds')
-
-figure;
-plot(ceilo.time,cloudHeight)
-xlabel('Time UTC')
-ylabel('Height of clouds [m]')
-title('Height of Clouds')
-datetick;
-%% percipitation
-
-figure;
-subplot(3,1,1);
-pcolor(ceilo.time,ceilo.range,log10(abs(0.75*(ceilo.RCS))));
-shading flat;
-colormap(jet);
-caxis([2.4 5.5]);
-%set(gca, 'YScale', 'log')
-datetick;
-ylim([0 3000]);
-percipitation = getpercipitation(ceilo);
-title('RCS in Height vs. Time')
->>>>>>> origin/percipitation
-
-subplot(3,1,2);
-area(ceilo.time,percipitation);
-datetick;
-title('Precipitation Presence')
-
-% grono percipitation data
-data.year   = []; % Jahr
-data.month  = []; % Monat
-data.day    = []; % Tag
-data.hour   = []; % Stunde
-data.min    = []; % Minute
-data.N93    = []; % 93 Niederschlag; Zehnminutensumme [mm]
-
-% path to Grono data
-filepath = pwd;
-filename = [filepath '/' 'GRONO_SWISSMETNET_all.dat'];
-
-% read data - first determining headerlines
-fid = fopen(filename);
-frewind(fid);
-c = textscan(fid,'%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f ','Headerlines',27);
-fclose(fid);
-data.year   = [data.year;  c{ 2}]; 
-data.month  = [data.month; c{ 3}];
-data.day    = [data.day;   c{ 4}]; 
-data.hour   = [data.hour;  c{ 5}]; 
-data.min    = [data.min;   c{ 6}]; 
-data.N93    = [data.N93;   c{12}]; 
-
-% set 'serial date time' for each messurement
-data.time = datenum(data.year,data.month,data.day,data.hour,data.min,0);
 
 
-subplot(3,1,3);
-startdate = list_dates;
-enddate   = list_dates+1;
-
-% ev. transformation of grono data in utc time
-% if startdate < 737143             %solar time
-%     data.hour=data.hour+1;
-% elseif startdate == 737143        %24.03 change of time at 02:00
-%     if data.hour < 2
-%         data.hour=data.hour+1;
-%     else
-%         data.hour=data.hour+2;
-%     end    
-% else                              %legal time
-%     data.hour=data.hour+2;
-% end
-% 
-% data.time = datenum(data.year,data.month,data.day,data.hour,data.min,0);
-
-index_start=find(data.time==startdate);
-index_end=find(data.time==enddate);
-plot(data.time(index_start:index_end), data.N93(index_start:index_end));
-xlabel('Local Time')
-ylabel('Precipitation, 10 minutes mean [mm]')
-title('Precipitation in Grono Station')
-datetick;
 %% height averaging
-low_profile_value = nanmean(ceilo.RCS(45:55,:),1);
+low_profile_value = nanmean(ceilo.RCS(1:5,:),1);
 
 figure;
 plot(ceilo.time,low_profile_value);
