8b1e7d656fc5661e01149b699daf8a73ef7cb04f
killian <kbrennan@student.ethz.ch>
2018-05-14 13:10:03 +0200

    changed fitlm to ’RobustOpts’,’bisquare’

--- scatterplots.m
+++ scatterplots.m
@@ -48,29 +48,47 @@ data.percipitationMask = data.percipitation>=0.5;
 for i = 1:10
     data.humidityMask(i,:) = data.humidity>(i-1)*10 & data.humidity<=(i)*10;
 end
-%% scatterplots
+%% scatterplots RHs
 figure('units','normalized','outerposition',[0 0 1 1])
 model = cell(20,1);
-
+ploti = 0;
 for i = 1:20
-    subplot(4,5,i);
-    if i <= 10
+    if i <= 10 % no percipitation
         mask = data.humidityMask(mod(i,10)+1,:) & ~data.percipitationMask;
-    else
+        ploting = true;
+    else % percipitation
         mask = data.humidityMask(mod(i,10)+1,:) & data.percipitationMask;
+        if i>=16
+            ploting = true;
+        else
+            ploting = false;
+        end
+        
     end
     
-    scatter(data.RCS_low(mask),data.fmps(mask),'.');
-    ytickformat('%.1f');
-    try
-    model = fitlm(data.RCS_low(mask),data.fmps(mask));
-    plot(model);
-    titlestr = {[num2str((mod(i-1,10))*10) '-' num2str((mod(i-1,10)+1)*10) '% RH, R^2: ' num2str(round(model.Rsquared.Ordinary,2)) ],...
-        ['p-val: ' num2str(model.Coefficients.pValue(2,:))]};
-    catch
-    titlestr = [num2str((mod(i-1,10))*10) '-' num2str((mod(i-1,10)+1)*10) '% RH'];
+    if ploting
+        ploti = ploti+1;
+        subplot(3,5,ploti);
+        scatter(data.RCS_low(mask),data.fmps(mask));
+        %set(gca,'yscale','log','xscale','log');
+        
+        try
+            mdl = fitlm(data.RCS_low(mask),data.fmps(mask),'RobustOpts','bisquare');
+            model{i} = fitlm(data.RCS_low(mask),data.fmps(mask),'RobustOpts','bisquare');
+            plot(mdl);
+            titlestr = {[num2str((mod(i-1,10))*10) '-' num2str((mod(i-1,10)+1)*10) '% RH, R^2: ' num2str(round(model.Rsquared.Ordinary,3)) ],...
+                ['p-value: ' num2str(mdl.Coefficients.pValue(2,:))]};
+        catch
+            titlestr = [num2str((mod(i-1,10))*10) '-' num2str((mod(i-1,10)+1)*10) '% RH'];
+        end
+        
+        ytickformat('%.1f');
+        title(titlestr, 'Interpreter','tex');
+        hLeg = legend();
+        set(hLeg,'visible','off');
+        set(gca,'XLabel',[]);
+        set(gca,'YLabel',[]);
     end
-    title(titlestr);
 end
 
 % labeling
@@ -78,7 +96,7 @@ ax1 = axes('Position',[0 0 1 1],'Visible','off');
 axes(ax1) % sets ax1 to current axes
 
 % title
-text(.5,.98,'Ceilo RCS vs. FMPS backscatter','HorizontalAlignment','center','FontSize',30);
+text(.5,.98,'Ceilo RCS vs. FMPS backscatter, hourly','HorizontalAlignment','center','FontSize',30);
 
 % axis labels
 h = text(.05,.5,'FMPS backscatter [(nm^2)#/cm^3]','HorizontalAlignment','center','FontSize',20);
@@ -86,7 +104,31 @@ set(h,'Rotation',90);
 text(.5,.05,'Ceilo RCS','HorizontalAlignment','center','FontSize',20);
 
 % percipitation specifiers
-h = text(.1,.75,'no percipitation','HorizontalAlignment','center','FontSize',20);
+h = text(.1,.65,'no percipitation','HorizontalAlignment','center','FontSize',20);
 set(h,'Rotation',90);
-h = text(.1,.3,'percipitation','HorizontalAlignment','center','FontSize',20);
+h = text(.1,.22,'percipitation','HorizontalAlignment','center','FontSize',20);
 set(h,'Rotation',90);
+
+%% scatterplots percipitation
+figure;
+subplot(1,2,1);
+scatter(data.RCS_low(~data.percipitationMask),data.fmps(~data.percipitationMask));
+model = fitlm(data.RCS_low(~data.percipitationMask),data.fmps(~data.percipitationMask),'RobustOpts','bisquare');
+plot(model);
+ylabel('FMPS backscatter [(nm^2)#/cm^3]');
+xlabel('Ceilo RCS');
+title('no percipitation');
+hLeg = legend();
+set(hLeg,'visible','off');
+%set(gca, 'XScale', 'log')
+
+subplot(1,2,2);
+scatter(data.RCS_low(data.percipitationMask),data.fmps(data.percipitationMask));
+model = fitlm(data.RCS_low(data.percipitationMask),data.fmps(data.percipitationMask),'RobustOpts','bisquare');
+plot(model);
+set(gca,'YLabel',[]);
+xlabel('Ceilo RCS');
+title('percipitation');
+hLeg = legend();
+set(hLeg,'visible','off');
+%set(gca, 'XScale', 'log')
